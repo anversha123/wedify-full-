9746c148a6fac4a45e286d9e21a3024ffca86fa7
Anver <anvershajahan123@gmail.com>
2026-02-18 18:56:44 +0530

    Hardened WSGI path configuration for Render deployment

--- backend/wedify_project/wsgi.py
+++ backend/wedify_project/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add the project directory to sys.path so Gunicorn can find modules
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wedify_project.settings')
 
